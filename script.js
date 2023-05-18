d5d873ec68f5afef7abedd3618efdbd9ae86441e
Brian Sellers <bsellers3013@gmail.com>
2023-05-18 10:05:33 -0500

    added function, variables, and password length

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// set function and variables
+
 function generatePassword(){
-  return "hello"
+  return; hello
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G"]
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g"]
+  var isNumber = ["1", "2", "3", "4", "5", "6", "7"]
+  var userPickedCharacterSet = []
 }
+
+// set password length
+
+var passLength = parseInt(window.prompt("Please enter a number between 8 and 128 characters") )
+console.log(typeof passLength);
+
