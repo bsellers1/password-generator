6fbf8dabd873354393bfe0019da207e6ab942d1c
Brian Sellers <bsellers3013@gmail.com>
2023-05-18 17:15:55 -0500

    added variables for the password

--- script.js
+++ script.js
@@ -16,16 +16,16 @@ generateBtn.addEventListener("click", writePassword);
 
 // set function and variables
 
-function generatePassword(){
-  return; hello
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G"]
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g"]
-  var isNumber = ["1", "2", "3", "4", "5", "6", "7"]
-  var userPickedCharacterSet = []
-}
+function generatePassword() {
 
-// set password length
+// list out the variables for the password
 
-var passLength = parseInt(window.prompt("Please enter a number between 8 and 128 characters") )
-console.log(typeof passLength);
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var passNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var passSpec = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ]
+
+  }
