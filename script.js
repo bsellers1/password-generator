5e3e917ecbf1b23074d3c8fc5a6697fe8b89caca
Brian Sellers <bsellers3013@gmail.com>
2023-05-18 23:25:47 -0500

    able to make undefined appear but cant resolve the issue

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  var password = genPassword();
+  password = genPassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password
 }
 
-// Add event listener to generate button
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // picks a random one in the array
@@ -23,13 +23,13 @@ var newPassword = [];
 
 var passChoices = [];
 
-var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var passNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var passNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-var passSpec = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ];
+var passSpec = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ]
 
 function passCharacters() { // creates the prompts and arrays for the password
   if (confirm("Numbers?")) {
@@ -55,6 +55,7 @@ function passCharacters() { // creates the prompts and arrays for the password
 }
 
 function passLength() {
+
   var pLength = prompt("What number between 8 and 128 would you like?")
 
   if (pLength < 8 || pLength > 128) {
@@ -74,10 +75,13 @@ function passLength() {
   }
 
   return newPassword.join("");
+
 }
 
 function genPassword() {
+
   newPassword = [];
   passChoices = [];
-  return passLength();
+  passLength();
+  
 }
